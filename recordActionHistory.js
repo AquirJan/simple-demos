254e900d200bfe345a4908a0ec90341c23300efc
liangqj3 <liangqj3@midea.com>
2020-09-23 09:01:20 +0800

    优化历史记录

--- recordActionHistory.js
+++ recordActionHistory.js
@@ -1,46 +1,49 @@
 import cloneDeep from './lodash.clonedeep.js';
 export default class recordActionHistory {
   constructor(options) {
-    this.options = Object.assign({
-      recordName: '', // 是否使用特定名字
-      historyArray: [], // 操作记录的历史数据
-    }, cloneDeep(options));
-    this.revokedStep = 0
-    this.showoffHistoryArray = []
-    this.historyActionArray = []
-    this.init()
+    this.options = Object.assign(
+      {
+        recordName: '', // 是否使用特定名字
+        historyArray: [] // 操作记录的历史数据
+      },
+      cloneDeep(options)
+    );
+    this.revokedStep = 0;
+    this.showoffHistoryArray = [];
+    this.historyActionArray = [];
+    this.init();
   }
   // 后退
   revoke() {
     this.revokedStep = this.revokedStep + 1;
     if (this.revokedStep >= this.historyActionArray.length) {
-      this.revokedStep = this.historyActionArray.length - 1
+      this.revokedStep = this.historyActionArray.length - 1;
       return null;
     }
-    this.showoffHistoryArray.shift()
-    return this.showoffHistoryArray
+    this.showoffHistoryArray.shift();
+    return this.showoffHistoryArray;
   }
   // 前进
   onward() {
     this.revokedStep = this.revokedStep - 1;
     if (this.revokedStep < 0) {
-      this.revokedStep = 0
+      this.revokedStep = 0;
       return null;
     }
     if (!this.historyActionArray[this.revokedStep]) {
       this.revokedStep = this.revokedStep + 1;
-      return this.showoffHistoryArray
+      return this.showoffHistoryArray;
     }
-    this.showoffHistoryArray.unshift(this.historyActionArray[this.revokedStep])
-    return this.showoffHistoryArray
+    this.showoffHistoryArray.unshift(this.historyActionArray[this.revokedStep]);
+    return this.showoffHistoryArray;
   }
   // 获取显示的历史操作数组
   getHistoryArray() {
-    return cloneDeep(this.showoffHistoryArray)
+    return cloneDeep(this.showoffHistoryArray);
   }
   // 获取显示的历史操作数组长度
   getHistoryArrayLength() {
-    return this.showoffHistoryArray.length
+    return this.showoffHistoryArray.length;
   }
   // 获取最新一个
   getHistoryArrayFirst() {
@@ -48,27 +51,27 @@ export default class recordActionHistory {
   }
   // 获取当前步数
   getRevokedStep() {
-    return this.revokedStep
+    return this.revokedStep;
   }
   // 记录历史变化
   recordChange(data) {
-    this.historyActionArray.splice(this.revokedStep, 0, cloneDeep(data))
+    this.historyActionArray.splice(this.revokedStep, 0, cloneDeep(data));
     if (this.revokedStep !== 0) {
-      this.historyActionArray = this.historyActionArray.slice(-(this.historyActionArray.length - this.revokedStep))
+      this.historyActionArray = this.historyActionArray.slice(-(this.historyActionArray.length - this.revokedStep));
       this.revokedStep = 0;
     }
-    this.showoffHistoryArray = cloneDeep(this.historyActionArray)
+    this.showoffHistoryArray = cloneDeep(this.historyActionArray);
   }
   // 初始化
   init() {
-    this.historyActionArray = this.options.historyArray
+    this.historyActionArray = this.options.historyArray;
 
-    return this
+    return this;
   }
   destroy() {
-    this.options = null;
-    this.revokedStep = null;
-    this.showoffHistoryArray = null;
-    this.historyActionArray = null;
+    this.options.historyArray = [];
+    this.revokedStep = 0;
+    this.showoffHistoryArray = [];
+    this.historyActionArray = [];
   }
 }
